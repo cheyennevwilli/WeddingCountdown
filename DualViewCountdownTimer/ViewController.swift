d446eb1541d380d955fe4adebb9c2509a838b2cf
Cheyenne Vlymen-Williams <cheyennevwilliams@gmail.com>
2019-07-29 14:22:53 -0400

    Initial Commit

---
+++ DualViewCountdownTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DualViewCountdownTimer
+//
+//  Created by Cheyenne Vlymen-Williams on 7/29/19.
+//  Copyright © 2019 Cheyenne Vlymen-Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
